0a5e9ae31cce1743b9a748df9fd6a060ef7f814e
Cendekia Pramana Putra <me@cendekiapp.com>
2017-09-19 14:28:51 +0700

    [added] status bar height constant

--- constants/Layout.js
+++ constants/Layout.js
@@ -1,7 +1,9 @@
 import { Dimensions } from 'react-native';
+import { Constants } from 'expo';
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
+const STATUS_BAR_HEIGHT = Constants.statusBarHeight;
 
 export default {
   window: {
@@ -9,4 +11,5 @@ export default {
     height,
   },
   isSmallDevice: width < 375,
+  STATUS_BAR_HEIGHT
 };

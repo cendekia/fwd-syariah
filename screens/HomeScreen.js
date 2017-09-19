e631efe7cd8cb0586125f959c5a9c35200541ac7
Cendekia Pramana Putra <me@cendekiapp.com>
2017-09-19 14:29:58 +0700

    [modified] home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,207 +1,158 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Platform } from 'react-native';
 import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
+  Container,
+  Content,
   Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import { WebBrowser } from 'expo';
+  Card,
+  CardItem,
+  Body,
+  Title,
+  Badge,
+  Right
+} from "native-base";
 
-import { MonoText } from '../components/StyledText';
+import { Col, Row, Grid } from 'react-native-easy-grid';
+import Header from '../components/headers/DefaultHeader';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export default class HomeScreen extends React.Component {
+class HomeScreen extends Component {
   static navigationOptions = {
     header: null,
   };
 
   render() {
+    props = this.props;
+
     return (
-      <View style={styles.container}>
-        <ScrollView
-          style={styles.container}
-          contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
+      <Container>
+        <Header
+          title="FWD Life"
+          subTitle="Syariah"
+          {...props}
+        />
+        <Content style={{margin: 8}}>
+          <Row>
+            <Card>
+                <CardItem style={{ backgroundColor: "lightseagreen" }}>
+                  <Body>
+                    <Title style={{ fontSize: 20, color: "#fff" }}>Total Asset Syariah</Title>
+                    <Text style={{ fontSize: 14, color: "#fff" }}>IDR</Text>
+                    <Text style={{ fontSize: 50, color: "#fff", alignSelf: 'flex-end' }}>
+                      26.598
+                      <Text style={{ fontSize: 14, color: "#fff" }}>Mio</Text>
+                    </Text>
 
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View
-              style={[
-                styles.codeHighlightContainer,
-                styles.homeScreenFilename,
-              ]}>
-              <MonoText style={styles.codeHighlightText}>
-                screens/HomeScreen.js
-              </MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity
-              onPress={this._handleHelpPress}
-              style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>
-                Help, it didn’t automatically reload!
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>
-            This is a tab bar. You can edit it in:
-          </Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>
-              navigation/MainTabNavigator.js
-            </MonoText>
-          </View>
-        </View>
-      </View>
+                  </Body>
+                </CardItem>
+            </Card>
+          </Row>
+          <Row>
+            <Col>
+              <Card>
+                <CardItem style={{ backgroundColor: "#0082be" }}>
+                  <Body>
+                    <Title style={{ fontSize: 16, color: "#fff" }}>SBSN</Title>
+                    <Text style={{ fontSize: 12, color: "#fff" }}>IDR</Text>
+                    <Text style={{ fontSize: 25, color: "#fff", alignSelf: 'flex-end' }}>
+                      15.000
+                      <Text style={{ fontSize: 12, color: "#fff" }}>Mio</Text>
+                    </Text>
+                  </Body>
+                </CardItem>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <CardItem style={{ backgroundColor: "#EC644B" }}>
+                  <Body>
+                    <Title style={{ fontSize: 16, color: "#fff" }}>Rasio Solvabilitas</Title>
+                    <Text style={{ fontSize: 12, color: "#fff" }}>IDR</Text>
+                    <Text style={{ fontSize: 25, color: "#fff", alignSelf: 'flex-end' }}>
+                      48
+                      <Text style={{ fontSize: 12, color: "#fff" }}>%</Text>
+                    </Text>
+                  </Body>
+                </CardItem>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Card>
+                <CardItem style={{ backgroundColor: "#f65e1b" }}>
+                  <Body>
+                    <Title style={{ fontSize: 12, color: "#fff" }}>Fee (Ujrah)</Title>
+                    <Text style={{ fontSize: 7, color: "#fff" }}>IDR</Text>
+                    <Text style={{ fontSize: 15, color: "#fff", alignSelf: 'flex-end' }}>
+                      15.000
+                      <Text style={{ fontSize: 7, color: "#fff" }}>Mio</Text>
+                    </Text>
+                  </Body>
+                </CardItem>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <CardItem style={{ backgroundColor: "#fbbf15" }}>
+                  <Body>
+                    <Title style={{ fontSize: 12, color: "#fff" }}>Dana Peserta</Title>
+                    <Text style={{ fontSize: 7, color: "#fff" }}>IDR</Text>
+                    <Text style={{ fontSize: 15, color: "#fff", alignSelf: 'flex-end' }}>
+                      9.142
+                      <Text style={{ fontSize: 7, color: "#fff" }}>Mio</Text>
+                    </Text>
+                  </Body>
+                </CardItem>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <CardItem style={{ backgroundColor: "#999" }}>
+                  <Body>
+                    <Title style={{ fontSize: 12, color: "#fff" }}>Utang Lain</Title>
+                    <Text style={{ fontSize: 7, color: "#fff" }}>IDR</Text>
+                    <Text style={{ fontSize: 15, color: "#fff", alignSelf: 'flex-end' }}>
+                      2.456
+                      <Text style={{ fontSize: 7, color: "#fff" }}>Mio</Text>
+                    </Text>
+                  </Body>
+                </CardItem>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Card>
+              <CardItem style={{ backgroundColor: "#12afc0" }}>
+                <Body>
+                  <Title style={{ fontSize: 20, color: "#fff" }}>Reksa Dana Syariah</Title>
+                  <Text style={{ fontSize: 14, color: "#fff" }}>IDR</Text>
+                  <Text style={{ fontSize: 50, color: "#fff", alignSelf: 'flex-end' }}>
+                    9.239
+                    <Text style={{ fontSize: 14, color: "#fff" }}>Mio</Text>
+                  </Text>
+                </Body>
+              </CardItem>
+            </Card>
+          </Row>
+          <Row>
+            <Card>
+                <CardItem style={{ backgroundColor: "#1b4143" }}>
+                  <Body>
+                    <Title style={{ fontSize: 20, color: "#fff" }}>Aktiva (Non Inventasi)</Title>
+                    <Text style={{ fontSize: 14, color: "#fff" }}>IDR</Text>
+                    <Text style={{ fontSize: 50, color: "#fff", alignSelf: 'flex-end' }}>
+                      2.358
+                      <Text style={{ fontSize: 14, color: "#fff" }}>Mio</Text>
+                    </Text>
+                  </Body>
+                </CardItem>
+            </Card>
+          </Row>
+        </Content>
+      </Container>
     );
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use
-          useful development tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/development-mode'
-    );
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-});
+export default HomeScreen;

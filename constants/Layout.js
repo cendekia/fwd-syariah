91b0982172198ad7285a5380f6f0b4bf5dd6f2c9
Cendekia Pramana Putra <me@cendekiapp.com>
2017-09-20 11:47:00 +0700

    [modified] split contants

--- constants/Layout.js
+++ constants/Layout.js
@@ -1,15 +1,12 @@
 import { Dimensions } from 'react-native';
-import { Constants } from 'expo';
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
-const STATUS_BAR_HEIGHT = Constants.statusBarHeight;
 
 export default {
   window: {
     width,
     height,
   },
-  isSmallDevice: width < 375,
-  STATUS_BAR_HEIGHT
+  isSmallDevice: width < 375
 };

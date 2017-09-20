0fb79c22d774fbd940d218131fe8217bcda8b8b9
Cendekia Pramana Putra <me@cendekiapp.com>
2017-09-20 15:13:20 +0700

    [prepared] products screen

--- screens/ProductsScreen.js
+++ screens/ProductsScreen.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { Container } from "native-base";
+import Header from '../components/headers/DefaultHeader';
+
 
 export default class ProductsScreen extends React.Component {
   static navigationOptions = {
-    title: 'Products',
+    header: <Header
+      title="Products"
+      subTitle="FWD Life Syariah"
+      {...this.props}
+    />,
   };
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+      <Container>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
-});

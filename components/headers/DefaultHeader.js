93b72e7e6df0d17ebf533bf388834b8e2c475f31
Cendekia Pramana Putra <me@cendekiapp.com>
2017-09-20 11:48:43 +0700

    [modified] adjust default header

--- components/headers/DefaultHeader.js
+++ components/headers/DefaultHeader.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { STATUS_BAR_HEIGHT } from '../../constants/Layout';
+import { STATUS_BAR_HEIGHT } from '../../constants';
+import { Ionicons } from '@expo/vector-icons';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {
   Container,
@@ -23,8 +24,8 @@ class DefaultHeader extends Component {
       container: {
         height: Platform.OS ==='android' ? 25 : 45
       },
-      iconName: "account",
-      fontSize: 25,
+      iconName: "ios-contact",
+      fontSize: 30,
       icon: {
         color: "#fff"
       }
@@ -41,14 +42,14 @@ class DefaultHeader extends Component {
     }
     function renderButton(state) {
       return  <Button transparent onPress={state.action}>
-                <Icon name={state.style.iconName} size={state.style.fontSize} style={state.style.icon} />
+                <Ionicons name={state.style.iconName} size={state.style.fontSize} style={state.style.icon} />
               </Button>;
     }
 
     return (
       <Header
         style={{
-          height: Platform.OS ==='android' ? 54 + STATUS_BAR_HEIGHT : 65
+          height: Platform.OS ==='android' ? 30 + STATUS_BAR_HEIGHT : 65
         }}
         androidStatusBarColor="#ff8200"
         backgroundColor="#ff8200"
@@ -57,7 +58,7 @@ class DefaultHeader extends Component {
         { !this.props.leftButton && renderLeft(this.defaultState)}
         { this.props.leftButton && renderLeft(this.props.leftButton)}
 
-        <Body style={{ height: Platform.OS ==='android' ? 25 : 35 }}>
+        <Body style={{ height: Platform.OS ==='android' ? 25 + STATUS_BAR_HEIGHT : 35 }}>
           <Title style={{ color: "#FFF" }}>{this.props.title}</Title>
           <Subtitle style={{ color: "#FFF" }}>{this.props.subTitle}</Subtitle>
         </Body>

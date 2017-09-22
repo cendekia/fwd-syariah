dced769bc164d7ee42edfbe81542cea99ac35860
Cendekia Pramana Putra <me@cendekiapp.com>
2017-09-22 18:07:20 +0700

    [add] customer care common question

--- screens/HelpScreen.js
+++ screens/HelpScreen.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
-import { Container } from "native-base";
+import { TouchableOpacity } from 'react-native';
+import {
+  Container,
+  Content,
+  Card,
+  CardItem,
+  Body,
+  Text,
+  Right,
+  Button
+} from "native-base";
 import Header from '../components/headers/DefaultHeader';
+import { Ionicons } from '@expo/vector-icons';
 
 export default class HelpScreen extends Component {
   static navigationOptions = {
@@ -15,6 +26,78 @@ export default class HelpScreen extends Component {
   render() {
     return (
       <Container>
+        <Content style={{margin: 8}}>
+          <Card>
+            <CardItem>
+              <Body>
+                <Text>
+                  Apa itu FWD Life Sharia?
+                </Text>
+              </Body>
+              <Right>
+                <TouchableOpacity>
+                  <Ionicons name="ios-arrow-forward" size='20' />
+                </TouchableOpacity>
+              </Right>
+            </CardItem>
+          </Card>
+          <Card>
+            <CardItem>
+              <Body>
+                <Text>
+                  Apakah yang dimaksud dengan 'tertanggung'?
+                </Text>
+              </Body>
+              <Right>
+                <TouchableOpacity>
+                  <Ionicons name="ios-arrow-forward" size='20' />
+                </TouchableOpacity>
+              </Right>
+            </CardItem>
+          </Card>
+          <Card>
+            <CardItem>
+              <Body>
+                <Text>
+                  Apakah yang dimaksud dengan 'ahli waris'?
+                </Text>
+              </Body>
+              <Right>
+                <TouchableOpacity>
+                  <Ionicons name="ios-arrow-forward" size='20' />
+                </TouchableOpacity>
+              </Right>
+            </CardItem>
+          </Card>
+          <Card>
+            <CardItem>
+              <Body>
+                <Text>
+                  Apakah yang dimaksud dengan 'pemegang polis'?
+                </Text>
+              </Body>
+              <Right>
+                <TouchableOpacity>
+                  <Ionicons name="ios-arrow-forward" size='20' />
+                </TouchableOpacity>
+              </Right>
+            </CardItem>
+          </Card>
+          <Card>
+            <CardItem>
+              <Body>
+                <Text>
+                  Apa beda FWD Life Sharia dengan asuransi konvensional lainnya?
+                </Text>
+              </Body>
+              <Right>
+                <TouchableOpacity>
+                  <Ionicons name="ios-arrow-forward" size='20' />
+                </TouchableOpacity>
+              </Right>
+            </CardItem>
+          </Card>
+        </Content>
       </Container>
     );
   }
